5a39738204f8827117eedc2f153fb4c5c8266aa0
gejt <852442493@qq.com>
2021-01-24 23:28:36 +0800

    defaul fallback and service fallback

--- zuul-server/src/main/java/xyz/zao123/zuul/ZuulBootstrap.java
+++ zuul-server/src/main/java/xyz/zao123/zuul/ZuulBootstrap.java
@@ -3,6 +3,10 @@ package xyz.zao123.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Bean;
+import xyz.zao123.zuul.fallback.DefaultFallbackProvider;
+import xyz.zao123.zuul.fallback.MyFallbackProvider;
 
 /**
  * @author gejt
@@ -12,5 +16,15 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(ZuulBootstrap.class,args);
+    }
+
+//    @Bean
+//    public FallbackProvider defaulFallbackProvider(){
+//        return new DefaultFallbackProvider();
+//    }
+
+    @Bean
+    public FallbackProvider apiFallbackProvider(){
+        return new MyFallbackProvider();
     }
 }
